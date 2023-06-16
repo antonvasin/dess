6b1f50c99d9e7136b427053b274c75e68ab49e3e
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-06-16 21:35:34 +0100

    cleanup

--- mod.tsx
+++ mod.tsx
@@ -1,26 +1,28 @@
-import { html, tokens } from "https://deno.land/x/rusty_markdown@v0.4.1/mod.ts";
-import { extract, test } from "https://deno.land/std@0.192.0/front_matter/any.ts";
 import { parse } from "https://deno.land/std@0.192.0/flags/mod.ts";
-import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.37/mod.ts";
 import { exists } from "https://deno.land/std@0.192.0/fs/mod.ts";
 
+import { html, tokens } from "https://deno.land/x/rusty_markdown@v0.4.1/mod.ts";
+import { extract, test } from "https://deno.land/std@0.192.0/front_matter/any.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.37/mod.ts";
+
 /*
- * [ ]
- *     [ ]
- *
  * [x] Collect md files
  * [ ] Process md files
  *     [x] parse markdown
  *     [x] read frontmatter
  *     [x] get html
  *     [x] replace links
- *         [ ] prepend with absolute path
+ *         [x] prepend with absolute path
+ *         [ ] handle .md links
+ *         [ ] handle nested links
+ *     [ ] create url-safe anchors for headers
+ *     [ ] return list of headers for a page
  * [ ] Render static files
  *     [ ] apply layout
  *         [ ] read jsx file from frontmatter
  *         [x] render html
  *         [x] insert content into html
- *     [ ] add asset scrpits
+ *     [ ] add asset scripts
  *         [ ] read files from frontmatter
  *         [ ] add <script> to page
  *         [ ] copy asset to /dist
@@ -79,7 +81,7 @@ async function collect(dir: string) {
   return posts;
 }
 
-async function processMd(file: string, pages: string[]) {
+async function processMd(file: string, routes: string[]) {
   try {
     const md = await Deno.readTextFile(file);
     let frontmatter: Record<string, unknown> | undefined;
@@ -91,18 +93,19 @@ async function processMd(file: string, pages: string[]) {
     }
 
     const parsed = tokens(body);
-    console.log("Before rewrite");
-    console.dir(pages);
-    console.table(parsed);
+    // console.log("Before rewrite");
+    // console.table(parsed);
     for (let i = 0; i < parsed.length; i++) {
       const token = parsed[i];
-      if (token.type === "start" && token.tag === "link" && pages.includes(token.url)) {
+      if (token.type === "start" && token.tag === "link" && routes.includes(token.url)) {
+        // TODO: use URL to preserve query string
+        // TODO: Handle `.md` links
         token.url = baseUrl + token.url + ".html";
       }
     }
 
-    console.log("After rewrite");
-    console.table(parsed);
+    // console.log("After rewrite");
+    // console.table(parsed);
     return { html: html(parsed), frontmatter };
   } catch (err) {
     console.error(
@@ -117,11 +120,13 @@ export async function createHTML({ srcDir = collectionDir, out = outDir } = {}) 
     await Deno.remove(outDir, { recursive: true });
   }
   await Deno.mkdir(outDir);
-  // for await (const file of await collect(srcDir)) {
-  (await collect(srcDir)).forEach(async (file, _i, ary) => {
+  const files = await collect(srcDir);
+  const routes = files.map((f) => "/" + f.name.replace(/\.md$/i, ""));
+
+  for await (const file of files) {
     const { html, frontmatter } = await processMd(
       `${srcDir}/${file.name}`,
-      ary.map((f) => "/" + f.name.replace(/\.md$/i, "")),
+      routes,
     );
 
     if (frontmatter) {
@@ -138,5 +143,5 @@ export async function createHTML({ srcDir = collectionDir, out = outDir } = {}) 
     );
 
     Deno.writeTextFile(`${out}/${file.name.replace(/\.md$/, ".html")}`, rendered);
-  });
+  }
 }
