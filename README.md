19ead3c0170f58503f02ad68ee8e5a445069ab6c
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-07-15 15:51:20 +0100

    move tasks to readme

--- README.md
+++ README.md
@@ -25,3 +25,36 @@ await build({
   layout: MyCustomLayout,
 });
 ```
+
+## TODO
+
+- [x] Collect md files
+  - [ ] collect html files
+  - [ ] collect custom 404 page
+  - [ ] skip draft pages
+- [ ] Process md files
+  - [x] parse markdown
+  - [x] read frontmatter
+  - [x] get html
+  - [x] replace links
+    - [x] prepend with absolute path
+    - [ ] handle .md links
+    - [x] handle nested links
+    - [x] preserve query strings and headers
+  - [x] create url-safe anchors for headers
+  - [x] return list of headers for a page
+  - [x] return meta from frontmatter
+- [ ] Render static files
+  - [x] apply layout
+    - [x] read jsx file from frontmatter
+    - [x] render html
+    - [x] insert content into html
+  - [ ] add asset scripts
+    - [ ] read files from frontmatter
+    - [ ] add <script> to page
+    - [ ] copy asset to /dist
+  - [x] render final html to file
+- [x] Copy to dist/ folder
+- [x] copy public files
+- [x] HMR
+- [ ] RSS
--- mod.ts
+++ mod.ts
@@ -24,38 +24,6 @@ import { serve as _serve, ServeInit } from "https://deno.land/std@0.192.0/http/s
 import { DefaultLayout, LayoutComponent, NotFound } from "./Components.tsx";
 import { Status } from "https://deno.land/std@0.192.0/http/http_status.ts";
 
-/*
- * [x] Collect md files
- *     [ ] collect html files
- *     [ ] collect custom 404 page
- *     [ ] skip draft pages
- * [ ] Process md files
- *     [x] parse markdown
- *     [x] read frontmatter
- *     [x] get html
- *     [x] replace links
- *         [x] prepend with absolute path
- *         [ ] handle .md links
- *         [x] handle nested links
- *         [x] preserve query strings and headers
- *     [x] create url-safe anchors for headers
- *     [x] return list of headers for a page
- *     [x] return meta from frontmatter
- * [ ] Render static files
- *     [x] apply layout
- *         [x] read jsx file from frontmatter
- *         [x] render html
- *         [x] insert content into html
- *     [ ] add asset scripts
- *         [ ] read files from frontmatter
- *         [ ] add <script> to page
- *         [ ] copy asset to /dist
- *     [x] render final html to file
- * [x] Copy to dist/ folder
- * [x] copy public files
- * [x] HMR
- */
-
 const extensions = [".md"];
 const publicDir = "public";
 const ignoreNames = [
